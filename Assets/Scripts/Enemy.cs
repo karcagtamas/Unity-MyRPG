050860a4b14a5ea17543b6eae0b995d94e2113da
Karcag Tamas <karcagtamas@outlook.com>
2026-02-02 14:27:19 +0100

    Timers & Cooldowns

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,37 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    [SerializeField] private float redColorDuration = 1;
+    public float currentTimeInGame;
+    public float lastTimeWasDamaged;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        ChangeColorIfNeeded();
+    }
+
+    private void ChangeColorIfNeeded()
+    {
+        currentTimeInGame = Time.time;
+
+        if (currentTimeInGame > lastTimeWasDamaged + redColorDuration)
+        {
+            if (sr.color != Color.white)
+            {
+                sr.color = Color.white;
+            }
+        }
+    }
+
     public void TakeDamage()
     {
-        
+        sr.color = Color.red;
+        lastTimeWasDamaged = Time.time;
     }
 }
